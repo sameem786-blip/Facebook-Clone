b2cb92848b9c56505b6cda1f5748c062bb5ef9ae
sameem786-blip <70144568+sameem786-blip@users.noreply.github.com>
2023-03-07 00:15:08 +0500

    post finalised

--- client/components/other/publicPosts/publicPosts.js
+++ client/components/other/publicPosts/publicPosts.js
@@ -5,6 +5,21 @@ import Image from 'next/image';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import CloseIcon from '@mui/icons-material/Close';
 import RecommendIcon from '@mui/icons-material/Recommend';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import MessageIcon from '@mui/icons-material/Message';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
+import GroupIcon from '@mui/icons-material/Group';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import RecentActorsIcon from '@mui/icons-material/RecentActors';
+import GroupsIcon from '@mui/icons-material/Groups';
+import StoreIcon from '@mui/icons-material/Store';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import HistoryIcon from '@mui/icons-material/History';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import VideoCameraFrontRoundedIcon from '@mui/icons-material/VideoCameraFrontRounded';
+import CollectionsRoundedIcon from '@mui/icons-material/CollectionsRounded';
+import MoodRoundedIcon from '@mui/icons-material/MoodRounded';
 
 function publicPosts() {
     return (
@@ -30,14 +45,37 @@ function publicPosts() {
             </div>
             <div className={styles.postMedia}>
                 <Image src={postPool[0].postMedia} width={500} height={500} className={styles.postMediaPic} />
+
             </div>
             <div className={styles.postReactions}>
                 <div className={styles.postLikes}>
-                    <RecommendIcon />
+                    <RecommendIcon className={styles.postLikeIcon} />
+                    <p clas>{postPool[0].postLikes}</p>
                 </div>
                 <div className={styles.postCommentShares}>
                     <p className={styles.comments}>{postPool[0].postComments} comments</p>
                     <p className={styles.shares}>{postPool[0].shares} shares</p>
+                </div>
+            </div>
+            <div className={styles.postLineBreak}></div>
+            <div className={styles.postFooter}>
+                <div className={styles.dashboardCreatePostActions}>
+                    <ThumbUpIcon className={styles.postLikeBtn} />
+                    <div className={styles.dashboardCreatePostActionsDesc}>
+                        Likes
+                    </div>
+                </div>
+                <div className={styles.dashboardCreatePostActions}>
+                    <MessageIcon className={styles.postCommentBtn} />
+                    <div className={styles.dashboardCreatePostActionsDesc}>
+                        Comments
+                    </div>
+                </div>
+                <div className={styles.dashboardCreatePostActions}>
+                    <NotificationsIcon className={styles.postSendBtn} />
+                    <div className={styles.dashboardCreatePostActionsDesc}>
+                        Share
+                    </div>
                 </div>
             </div>
         </>
